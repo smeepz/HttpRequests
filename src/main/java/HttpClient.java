566c24bf0b8459b5d7415e4e84d9246766202864
smeepz <stianlw@gmail.com>
2020-09-09 13:10:47 +0200

    Mad fixes 1.1

--- src/main/java/HttpClient.java
+++ src/main/java/HttpClient.java
@@ -2,26 +2,31 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class HttpClient {
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("urlecho.appspot.com", 80);
-        String request =
-                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\n" +
-                "\rAccept-Encoding: gzip, deflate\n" +
-                "\rAccept-Language: en-GB,en-US;q=0.9,en;q=0.8\n" +
-                "\rCache-Control: max-age=0\n" +
-                "\rConnection: keep-alive\n" +
-                "\rHost: urlecho.appspot.com\n" +
-                "\rUpgrade-Insecure-Requests: 1\n" +
-                "\rUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36";
-        socket.getOutputStream().write(request.getBytes());
 
-        int c;
-        while ((c = socket.getInputStream().read()) != -1) {
-            System.out.print((char) c);
-        }
+    private final String host;
+    private final int port;
+    private final String requestTarget;
+
+    public static void main(String[] args) throws IOException {
+        new HttpClient("urlecho.appspot.com", 80, "/echo").executeRequest();
     }
 
-    public int getStatusCode() {
-        return 0;
+    public HttpClient(String host, int port, String requestTarget) {
+        this.host = host;
+        this.port = port;
+        this.requestTarget = requestTarget;
+    }
+
+    private void executeRequest() throws IOException {
+        // new Socket opens a connection to the server
+        Socket socket = new Socket(host, port);
+        //  The outputstream of the client is connected to the input stream of the server
+        socket.getOutputStream().write(("GET " + requestTarget + " HTTP/1.1\r\n" +
+                "Host: " + host + "\r\n" +
+                "\r\n").getBytes());
+        int c;
+        while ((c = socket.getInputStream().read()) != -1) {
+            System.out.print((char)c);
+        }
     }
 }
---
+++ src/test/java/HttpClientTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+
+   /* @Test
+    void shouldGetSuccessfulResponseCode() {
+            HttpClient client = new HttpClient("urlecho.appspot.com", 80, "/echo");
+            assertEquals(200, client.getStatusCode());
+    }*/
+
+}
+
+
